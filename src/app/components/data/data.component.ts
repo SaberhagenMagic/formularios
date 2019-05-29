bc5d498f5311ba3863a93ea1be75d09f2491f20c
SaberhagenMagic <ray_18m_2000@yahoo.com>
2019-05-29 13:22:28 -0500

    first commit

---
+++ src/app/components/data/data.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styles: []
+})
+export class DataComponent {
+
+  forma: FormGroup;
+
+  constructor() {
+    this.forma = new FormGroup({
+      'nombre': new FormControl('', [Validators.required, Validators.minLength(5)]),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+    });
+  }
+
+  guardar() {
+    console.log(this.forma.value);
+  }
+
+}
